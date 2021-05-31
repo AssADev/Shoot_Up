413f11dbf762d2b659c7564378bfb981990c4aaa
AssA <gravelteo.pro@gmail.com>
2021-05-31 15:22:28 +0200

    Volume slider Added

---
+++ Assets/Scripts/MenuScripts/SoundManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour {
+    // Variables :
+    public Slider volumeSlider;
+
+    void Start() {
+        if (!PlayerPrefs.HasKey("musicVolume")) {
+            PlayerPrefs.SetFloat("musicVolume", 0.75f);
+            LoadVolume();
+        } else {
+            LoadVolume();
+        }
+    }
+
+    public void ChangeVolume() {
+        AudioListener.volume = volumeSlider.value;
+        SaveVolume();
+    }
+
+    private void LoadVolume() {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void SaveVolume() {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
